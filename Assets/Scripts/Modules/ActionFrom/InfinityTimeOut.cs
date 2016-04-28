8711cc7ea597504e9eab70a69a85faf1316abf57
ComanGames <alfaa.gen@gmail.com>
2016-04-28 20:22:05 +0300

    Last modules for monkey scene

---
+++ Assets/Scripts/Modules/ActionFrom/InfinityTimeOut.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Modules.ActionFrom
+{
+    public class InfinityTimeOut : Worker
+    {
+        public float FirstTimeOut = 5f;
+        public float SecondTimeOut = 2f;
+        public bool WorkOnFirst = false;
+
+        public void Start()
+        {
+            StartCoroutine(InfinityWork());
+        }
+
+        private IEnumerator InfinityWork()
+        {
+            if (WorkOnFirst)
+            {
+                IsWorking = true;
+                yield return new WaitForSeconds(FirstTimeOut);
+            }
+            else
+            {
+               yield return new WaitForSeconds(FirstTimeOut);
+            }
+            while (true)
+            {
+                if (WorkOnFirst)
+                {
+                    if (!IsWorking)
+                    {
+                        IsWorking = true;
+                        yield return new WaitForSeconds(FirstTimeOut);
+                    }
+                    else if (IsWorking)
+                    {
+                        IsWorking = false;
+                        yield return new WaitForSeconds(SecondTimeOut);
+                    }
+                }
+                // if WorkOnFirst = false;
+                else
+                {
+                    if (!IsWorking)
+                    {
+                        IsWorking = true;
+                        yield return new WaitForSeconds(SecondTimeOut);
+                    }
+                    else if (IsWorking)
+                    {
+                        IsWorking = false;
+                        yield return new WaitForSeconds(FirstTimeOut);
+                    }
+
+                }
+            } 
+        }
+    }
+}
---
+++ Assets/Scripts/Modules/ActionFrom/TitleShower.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Modules.ActionFrom
+{
+    public class TitleShower : MonoBehaviour
+    {
+        public Worker ActionHolder;
+        public Text TextToWork;
+
+        public void Update()
+        {
+            TextToWork.enabled = ActionHolder.Work();
+        }
+    }
+}

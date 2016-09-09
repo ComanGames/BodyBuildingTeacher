cc10703ab1195e22c9acab539eaf7f0393853dd7
ComanGames <alfaa.gen@gmail.com>
2016-09-09 21:24:20 +0300

    Add ads and new icnons

---
+++ Assets/Scripts/Mathematic/Ads.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+namespace Assets.Scripts.Mathematic
+{
+    public class Ads : MonoBehaviour
+    {
+        //public variables
+        public static Ads Instance;
+         
+        //private variables
+        public void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+
+        public void StartAds()
+        {
+            StartCoroutine(ShowAdWhenReady());
+        }
+        IEnumerator ShowAdWhenReady()
+        {
+            while (!Advertisement.IsReady())
+            {
+                Debug.Log($"Is ads Initilized {Advertisement.isInitialized}"); 
+                Debug.Log($"Is ads Ready {Advertisement.IsReady()}");
+                yield return null;
+                
+            }
+            if (Advertisement.IsReady())
+            {
+                Advertisement.Show();
+            }
+        }
+    }
+}
